8e9dcb7707baa83497a872861443206414e35b6b
Syed-A-Shah <ssyed1559@gmail.com>
2019-08-01 23:52:00 +0100

    three tests written with failures

---
+++ spec/account_spec.rb
@@ -0,0 +1,28 @@
+require 'account'
+
+describe Account do
+  let(:account) { described_class.new }
+
+  describe 'When instantiated' do
+    it 'account has a balance of 0' do
+      expect(account.balance).to eq(0)
+    end
+  end
+
+  describe '#deposit' do
+    it 'user can deposit into the account' do
+      account.deposit(10)
+      expect(account.balance).to eq(10)
+    end
+  end
+
+  describe '#withdraw' do
+   it 'user can withdraw from the account' do
+     account.deposit(20)
+     account.withdraw(10)
+     expect(account.balance).to eq(10)
+   end
+ end
+
+
+ end
